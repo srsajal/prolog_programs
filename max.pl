049b32d4edd19d6179bc6e1c1139ef710a8d32b8
Sajal Giri <139799565+srsajal@users.noreply.github.com>
2025-01-20 13:38:56 +0530

    4 programs added

---
+++ max.pl
@@ -0,0 +1,14 @@
+max(X,Y) :-
+    (
+        X = Y ->
+        write('both are equal')
+        ;
+        X > Y ->
+        (
+            Z is X,
+            write(Z)    
+            )
+            ;
+            Z is Y,
+            write(Z)
+    ).
